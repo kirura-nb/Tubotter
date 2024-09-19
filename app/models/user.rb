b576692246cc026566740973d0a694bd21355a5e
kirura-nb <kikurage.taka@icloud.com>
2024-09-19 10:44:31 +0900

    ログイン機能～完成～

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :name, presence: true, length: {minimum:2, maximum:8}
+    validates :name, presence: true, length: {minimum:2, maximum:16}
     validates :s_number, presence: true, uniqueness: true
     validates :password, presence: true, format: {with: /\A[0-9]+\z/, multiline: true}
 end
