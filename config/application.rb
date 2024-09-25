20b81de4f6948407e02e09409300d50c35b1bef9
kirura-nb <kikurage.taka@icloud.com>
2024-09-25 23:33:53 +0900

    投稿機能がほぼ完成（返信、RT、いいね機能が未実装）

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Tubotter
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
   end
 end
